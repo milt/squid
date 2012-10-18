7dd9d27727f0579434441022ec957d5c83935137
Milton Reder <milt@jhu.edu>
2012-10-18 16:42:04 -0400

    fixed migrations, started fixtures

--- db/migrate/003_create_skill_levels.rb
+++ db/migrate/003_create_skill_levels.rb
@@ -3,6 +3,7 @@ class CreateSkillLevels < ActiveRecord::Migration
     create_table :skill_levels do |t|
       t.integer :user_id
       t.integer :skill_id
+      t.integer :rating
     end
   end
 end
