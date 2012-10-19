05c18e9353eb0a79bd6fecca0c3cf543dcca0b15
Milton Reder <milt@jhu.edu>
2012-10-19 11:55:41 -0400

    added some skill tests

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,4 +3,6 @@ class Skill < ActiveRecord::Base
   belongs_to :skill_category
   has_many :skill_levels
   has_many :users, :through => :skill_levels
+  validates :name, :presence => true, :uniqueness => true
+  validates :skill_category, :presence => true
 end
--- test/unit/skill_test.rb
+++ test/unit/skill_test.rb
@@ -3,7 +3,18 @@ require File.expand_path('../../test_helper', __FILE__)
 class SkillTest < ActiveSupport::TestCase
 
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  test "skills should have names" do
+    skill = Skill.new(skill_category_id: 1)
+    assert !skill.save
+  end
+
+  test "skills should have categories" do
+    skill = Skill.new(name: "3d")
+    assert !skill.save
+  end
+
+  test "skill names should be unique" do
+    skill = Skill.new(name: "Video", skill_category_id: 2)
+    assert !skill.save
   end
 end
