7b0816809807579f274a12234e19a7009a71891b
Milton Reder <milt@jhu.edu>
2012-10-19 16:24:47 -0400

    validate range of skill level

--- app/models/skill_level.rb
+++ app/models/skill_level.rb
@@ -4,5 +4,5 @@ class SkillLevel < ActiveRecord::Base
   belongs_to :user
   validates :skill, presence: true
   validates :user, presence: true
-  validates :rating, presence: true, numericality: { only_integer: true }
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 4 }
 end
